51a3896b7c459dd4d4ca972cd818e0179975f7dc
Sergio <S.suarerez@gmail.com>
2020-07-03 13:40:32 -0500

    socket connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import http from "http";
+import socketio from "socket.io";
 import { config } from "dotenv";
 
 if (process.env.NODE_ENV === "development") {
@@ -10,6 +12,8 @@ if (process.env.NODE_ENV === "development") {
 import errorMiddleware from "./middlewares/errorMiddlerware";
 
 const app = express();
+const server = new http.Server(app);
+const io = socketio(server);
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -19,4 +23,4 @@ app.use("/products", productRouter);
 
 app.use(errorMiddleware);
 
-export default app;
+export { app, server, io };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import app from "./app";
+import { Socket } from "socket.io";
+import { server, io } from "./app";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+io.on("connection", (socket: Socket) => {
+  console.log("New user connected");
+
+  socket.on("join", (name: string) => {
+    console.log(name);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(PORT);
 console.log("Server running on http://localhost:5000");
